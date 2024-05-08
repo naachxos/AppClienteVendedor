01dcd714c983fbd980fdd3a921c984d7746cf041
nacho <ign.alvarezs@duocuc.cl>
2024-05-08 01:49:42 -0400

    cambios en configuracion se seguira editando esa pagina

--- src/app/paginas/cliente/configuracion/configuracion.page.ts
+++ src/app/paginas/cliente/configuracion/configuracion.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-configuracion',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracion.page.scss'],
 })
 export class ConfiguracionPage implements OnInit {
+  nombre: string = 'Juan';
+  edad: number = 25;
+  correo: string = 'algo@gmail.com'
+  editando: boolean = false;
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
   }
+
+  toggleEdicion() {
+    this.editando = !this.editando;
+  }
+
+  
+
+  guardarInfo() {
+    // Aquí puedes agregar la lógica para guardar la información en tu backend o donde sea necesario
+    console.log('Información guardada:', { nombre: this.nombre, edad: this.edad });
+    this.editando = false; // Finaliza la edición después de guardar
+  }
+
+  accederConfiguracion() {
+    // Aquí puedes redirigir a la página de configuración
+    this.navCtrl.navigateForward('/galeria-post');}
+
 
 }
